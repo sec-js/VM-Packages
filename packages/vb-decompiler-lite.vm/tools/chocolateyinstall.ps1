b6866cc46a55b6cb3cdd6e053418444b05f4dd6b
sara-rn <103417144+sara-rn@users.noreply.github.com>
2024-12-05 13:10:42 +0100

    Change executablePath to include $toolName
    Co-authored-by: Ana María Martínez Gómez <anamg@google.com>

--- packages/vb-decompiler-lite.vm/tools/chocolateyinstall.ps1
+++ packages/vb-decompiler-lite.vm/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ try{
     $zipSha256 = '099760dcae9daa4c83885a3817cf6f17442ce709de0105993bfbb4f17db87e62'
 
     $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
-    $executablePath = (Join-Path $toolDir "VB Decompiler.exe")
+    $executablePath = (Join-Path $toolDir "$toolName.exe")
     VM-Install-With-Installer $toolName $category "EXE" "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Dir=`"$($toolDir)`"" $executablePath $zipUrl -sha256 $zipSha256
 } catch {
     VM-Write-Log-Exception $_
